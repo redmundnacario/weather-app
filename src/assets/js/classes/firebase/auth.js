4e7026f1c18323baeaaaaaadf3ade0f553fc4e6a
redmundnacario <redmund.nacario@gmail.com>
2021-02-07 12:12:00 +0800

    logout added

--- src/assets/js/classes/firebase/auth.js
+++ src/assets/js/classes/firebase/auth.js
@@ -1,3 +1,5 @@
+// login
+
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,4 +16,13 @@ signupForm.addEventListener('submit', (e) => {
         M.Modal.getInstance(modal).close();
         signupForm.reset();
     });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
 });
