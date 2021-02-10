7280628bf5797019eb9184693586433cca50994a
redmundnacario <redmund.nacario@gmail.com>
2021-02-10 12:12:00 +0800

    added firestore db and get user data after login and signup

--- src/assets/js/classes/firebase/auth.js
+++ src/assets/js/classes/firebase/auth.js
@@ -4,19 +4,18 @@ firebase.initializeApp(firebaseConfig);
 
 // Make auth and firestore references
 const auth = firebase.auth();
-const db = firebase.firestore();
-// console.log(firebase)
-// console.log(auth)
-// console.log(db)
+const firestore = firebase.firestore();
 
 // // Firestore settings
 // // db.settings({timestampsInSnapshots: true});
 
 
 // listen for auth status changes
+
 auth.onAuthStateChanged(user => {
     if (user) {
       console.log('user logged in: ', user);
+
     } else {
       console.log('user logged out');
     }
@@ -25,7 +24,8 @@ auth.onAuthStateChanged(user => {
 
 // sign up
 const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
+
+signupForm.addEventListener('submit', async(e) => {
     e.preventDefault();
 
     // get user info
@@ -33,17 +33,34 @@ signupForm.addEventListener('submit', (e) => {
     const password = signupForm['signup-password'].value;
 
     // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const { user } = await auth.createUserWithEmailAndPassword(email, password).then(cred => {
+
         // console.log(cred.user);
         // close the signup modal & reset form
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
+
+        return cred
     });
+
+    let userData = await createUserProfileDocument(user,
+      {
+      locations : 
+        [{
+          sample: {
+            lat: null,
+            lon: null,
+            }
+        }]
+      }
+    )
+    console.log(userData)
 });
 
 // logout
 const logout = document.querySelector('#logout');
+
 logout.addEventListener('click', (e) => {
   e.preventDefault();
   auth.signOut()
@@ -51,7 +68,8 @@ logout.addEventListener('click', (e) => {
 
 // login
 const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
+
+loginForm.addEventListener('submit', async(e) => {
   e.preventDefault();
   
   // get user info
@@ -59,12 +77,62 @@ loginForm.addEventListener('submit', (e) => {
   const password = loginForm['login-password'].value;
 
   // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+  const { user } = await auth.signInWithEmailAndPassword(email, password).then((cred) => {
     // console.log(cred.user);
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
+    
+    return cred
   });
 
+  let userData = await createUserProfileDocument(user)
+  console.log(userData)
+
 });
+
+//
+const createUserProfileDocument = async(userAuth, additionalData) => {
+  // console.log(userAuth)
+  // if user profile from firebase auth is null/undefined,
+  // then exit function with no return value
+  if (!userAuth) return ;
+
+  // else get uid from the userAuth
+  // then use it for Query reference in firestore
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // The keyword await makes JavaScript wait until ...
+  // ...that promise settles and returns its result.
+  // await is only used inside async function
+  // this one get the document snapshot of the document ref. 
+  const snapShot = await userRef.get();
+  let userData;
+  // document snapshot has 'exists' boolean property
+  // if a User with specific uid doesn't exists in firestore ...
+  // then create new User account in firestore
+  if (!snapShot.exists){
+      // destructuring the userAuth
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+          await userRef.set({
+              displayName,
+              email,
+              createdAt,
+              ...additionalData
+          });
+
+          userData = await userRef.get()
+          userData = userData.data();
+
+      } catch(error){
+          console.log('Error in creating user in firestore', error.message);
+      }
+  } else {
+    userData = snapShot.data();
+  }
+  return userData;
+}
